dfd1ec1635c13d43fbeee45bf41aceb98f127865
MyC <nedelecgregoire@gmail.com>
2018-01-23 15:56:51 +0100

    deploy

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/MyC-register', // dev
-    database: 'mongodb://gregoire:charlotte>@ds113098.mlab.com:13098/mycharlotte',     // prod
+    database: "mongodb://'gregoire':'charlotte'>@ds113098.mlab.com:13098/mycharlotte",     // prod
     secret: 'mysecret'
 }
