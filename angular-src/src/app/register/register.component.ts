15014da58819e0f9f0d2b92b21bb2efcc45e79f4
MyC <nedelecgregoire@gmail.com>
2018-01-24 15:39:47 +0100

    css mobile

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -11,7 +11,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
   myForm: FormGroup;
