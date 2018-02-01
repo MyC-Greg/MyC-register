3f38f9bfd45da26cfab0704f6cee83aa9ebaddf7
MyC <nedelecgregoire@gmail.com>
2018-02-01 13:53:00 +0100

    server name mycharlotte.fr

--- angular-src/src/app/services/config.service.ts
+++ angular-src/src/app/services/config.service.ts
@@ -7,5 +7,5 @@ export class ConfigService {
     // serverURL = "http://192.168.178.179:3000/";
 
     // serverURL = 'http://localhost:3000/';
-    serverURL = 'https://mycharlotte.herokuapp.com/';
+    serverURL = 'http://www.mycharlotte.fr/';
 }
