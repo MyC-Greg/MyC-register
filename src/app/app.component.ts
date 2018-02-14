efd58dc1b38505e73e6fffb17a522f70af2e4a1a
MyC <nedelecgregoire@gmail.com>
2018-02-14 21:18:02 +0100

    add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      {name: 'title', content: 'MyCharlotte - Reduction des effets secondaires dus aux traitements du cancer du sein'},
+      // tslint:disable-next-line:max-line-length
+      {name: 'description', content: 'Programme basé sur la nutrition, l\'exercice physique et la gestion des émotions pour réduire les effets secondaires dûs aux chimiothérapies, radiothérapies, chirurgies, hormonothérapies et plus largement aux traitement du cancer du sein'},
+      {name: 'author', content: 'myCharlotte'},
+      {name: 'keywords', content: 'cancer, sein, chimiothérapie, radiothérapie, chirurgie, hormonothérapie'},
+      {name: 'robots', description: 'index, follow'}
+    ]);
+  }
+
   title = 'app';
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 import { User } from '../model/user.model';
 import { AuthService } from '../services/auth.service';
