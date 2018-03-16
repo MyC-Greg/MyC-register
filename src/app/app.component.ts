8c3cac002ae639f44099bebed343a96590ac0a53
MyC <nedelecgregoire@gmail.com>
2018-03-16 10:52:28 +0100

    coquille dans description et retrait de mycharlotte dans titre pour test SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ import { Meta, Title } from '@angular/platform-browser';
 export class AppComponent {
 
   constructor(private meta: Meta, title: Title) {
-    title.setTitle('myCharlotte - Cancer du sein: Réduction des effets secondaires dus aux traitements');
+    title.setTitle('Cancer du sein: Réduction des effets secondaires dus aux traitements');
 
     this.meta.addTags([
       // tslint:disable-next-line:max-line-length
-      {name: 'description', content: 'myCharlotte vous aide à gérer les effets secondaires des traitements du cancer du sein grâce à l\'alimentation, l\'exercice physique et la gestion des émotions. Vous êtes actrice de votre mieux--être pendant les chimiothérapies, les radiothérapies, la chirurgie et l\'hormonothérapie.'},
+      {name: 'description', content: 'myCharlotte vous aide à gérer les effets secondaires des traitements du cancer du sein grâce à l\'alimentation, l\'exercice physique et la gestion des émotions. Vous êtes actrice de votre mieux-être pendant les chimiothérapies, les radiothérapies, la chirurgie et l\'hormonothérapie.'},
       {name: 'author', content: 'myCharlotte'},
       {name: 'keywords', content: 'cancer, sein, chimiothérapie, radiothérapie, chirurgie, hormonothérapie'}
     ]);
